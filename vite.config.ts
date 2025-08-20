7a940a8d9e31a4fac5d9dd4cddc3ded5fa43b2ee
nikaloos <105426214+nickmartinwebdev@users.noreply.github.com>
2025-08-20 20:02:46 +0100

    Add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@/components': resolve(__dirname, './src/components'),
+      '@/pages': resolve(__dirname, './src/pages'),
+      '@/hooks': resolve(__dirname, './src/hooks'),
+      '@/utils': resolve(__dirname, './src/utils'),
+      '@/types': resolve(__dirname, './src/types'),
+      '@/schemas': resolve(__dirname, './src/schemas'),
+      '@/api': resolve(__dirname, './src/api'),
+    },
+  },
+})
